175e351ea2bb1decfb2a25b34929467f012e77ab
younes5zorro <younes5zorro@gmail.com>
2019-04-11 11:55:23 +0100

    connection to excel file complet

--- api/controller.py
+++ api/controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from sqlalchemy import create_engine
-
+import openpyxl
 
 advance_app = Blueprint('advance_app', __name__)
 
@@ -13,7 +13,7 @@ def version():
 
 
 @advance_app.route('/v1/connect', methods=['POST'])
-def connect():
+def connect_db():
     if request.method == 'POST':
         # Step 1: Extract POST data from request body as JSON
         json_data = request.get_json()
@@ -36,42 +36,26 @@ def connect():
                         engine.connect().execute("USE "+_DATABASE +" ;")
                         result["tabs"] = [tab for (tab,) in engine.execute("SHOW TABLES;")   ]
 
-                
+
         # # Step 5: Return the response as JSON
         return jsonify(result) 
 
 
-# @advance_app.route('/predict/classifier', methods=['POST'])
-# def predict_image():
-#     if request.method == 'POST':
-#         # Step 1: check if the post request has the file part
-#         if 'file' not in request.files:
-#             return jsonify('No file found'), 400
+@advance_app.route('/v1/file', methods=['POST'])
+def connect_file():
+    if request.method == 'POST':
 
-#         file = request.files['file']
+        print(request.files)
+        # Step 1: check if the post request has the file part
+        if 'file' not in request.files:
+            return jsonify('No file found'), 400
 
-#         # Step 2: Basic file extension validation
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
+        file = request.files['file']
 
-#             # Step 3: Save the file
-#             # Note, in production, this would require careful
-#             # validation, management and clean up.
-#             file.save(os.path.join(UPLOAD_FOLDER, filename))
+        result = {}
+        wb = openpyxl.load_workbook(file)
+        result["sheets"] = wb.get_sheet_names()
 
-#             _logger.debug(f'Inputs: {filename}')
+        # Step 5: Return the response as JSON
+        return jsonify(result) 
 
-#             # Step 4: perform prediction
-#             result = make_single_prediction(
-#                 image_name=filename,
-#                 image_directory=UPLOAD_FOLDER)
-
-#             _logger.debug(f'Outputs: {result}')
-
-#         readable_predictions = result.get('readable_predictions')
-#         version = result.get('version')
-
-#         # Step 5: Return the response as JSON
-#         return jsonify(
-#             {'readable_predictions': readable_predictions[0],
-#              'version': version})
