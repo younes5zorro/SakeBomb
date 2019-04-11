871fcd6f378aa03a71c110169e41905849d0bd7d
younes5zorro <younes5zorro@gmail.com>
2019-04-11 17:19:28 +0100

    connection file update Link insted of file direct

--- api/controller.py
+++ api/controller.py
@@ -1,6 +1,21 @@
 from flask import Blueprint, request, jsonify
 from sqlalchemy import create_engine
 import openpyxl
+import pathlib
+import os
+
+from werkzeug.utils import secure_filename
+
+import requests
+from io import BytesIO
+
+PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent.parent
+
+UPLOAD_FOLDER = PACKAGE_ROOT / 'uploads'
+UPLOAD_FOLDER.mkdir(exist_ok=True)
+
+MODELS_FOLDER = PACKAGE_ROOT / 'models'
+MODELS_FOLDER.mkdir(exist_ok=True)
 
 advance_app = Blueprint('advance_app', __name__)
 
@@ -44,18 +59,52 @@ def connect_db():
 @advance_app.route('/v1/file', methods=['POST'])
 def connect_file():
     if request.method == 'POST':
+        
+        json_data = request.get_json()
+        link  = json_data['link']
 
-        print(request.files)
+        file = requests.get(link).content
+        
         # Step 1: check if the post request has the file part
-        if 'file' not in request.files:
-            return jsonify('No file found'), 400
+        # if 'file' not in request.files:
+        #     return jsonify('No file found'), 400
 
-        file = request.files['file']
+        # file = request.files['file']
+
+        # filename = secure_filename(file.filename)
+
+        #     # Step 3: Save the file
+        #     # Note, in production, this would require careful
+        #     # validation, management and clean up.
+        # file.save(os.path.join(UPLOAD_FOLDER, filename))
 
         result = {}
-        wb = openpyxl.load_workbook(file)
+        wb = openpyxl.load_workbook(filename=BytesIO(file))
         result["sheets"] = wb.get_sheet_names()
 
         # Step 5: Return the response as JSON
         return jsonify(result) 
 
+
+# @advance_app.route('/v1/predict/regression', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         # Step 1: Extract POST data from request body as JSON
+#         json_data = request.get_json()
+#         _logger.debug(f'Inputs: {json_data}')
+
+#         # Step 2: Validate the input using marshmallow schema
+#         input_data, errors = validate_inputs(input_data=json_data)
+
+#         # Step 3: Model prediction
+#         result = make_prediction(input_data=input_data)
+#         _logger.debug(f'Outputs: {result}')
+
+#         # Step 4: Convert numpy ndarray to list
+#         predictions = result.get('predictions').tolist()
+#         version = result.get('version')
+
+#         # Step 5: Return the response as JSON
+#         return jsonify({'predictions': predictions,
+#                         'version': version,
+#                         'errors': errors})
--- run.py
+++ run.py
@@ -5,4 +5,4 @@ application = create_app()
 
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    application.run(host="0.0.0.0" ,debug=True)
