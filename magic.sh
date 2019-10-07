ac41a17480c45e893cbddec17c170a1c4297a88d
younes5zorro <younes5zorro@gmail.com>
2019-10-07 17:46:33 +0100

    add magic file

--- magic.sh
+++ magic.sh
@@ -1,1 +1,1 @@
-kill -9 $(sudo lsof -n -i :6010| grep LISTEN | awk '{print $2}') && virtualenv -p "/usr/bin/python3.6" dataone_env && source "dataone_env/bin/activate" && pip3 install -r requirements.txt && iptables -A INPUT -p tcp --dport 6001 -j ACCEPT && nohup python run.py > run.log &
+kill -9 $(sudo lsof -n -i :6001| grep LISTEN | awk '{print $2}') && virtualenv -p "/usr/bin/python3.6" dataone_env && source "dataone_env/bin/activate" && pip3 install -r requirements.txt && iptables -A INPUT -p tcp --dport 6001 -j ACCEPT && nohup python run.py > run.log &
