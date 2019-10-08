45081bcaa1013d18330af05fe45a33881a3bd76f
younes5zorro <younes5zorro@gmail.com>
2019-10-08 11:33:10 +0100

    add pm

--- magic.sh
+++ magic.sh
@@ -1,1 +1,1 @@
-virtualenv -p "/usr/bin/python3.6" dataone_env && source "dataone_env/bin/activate" && pip3 install -r requirements.txt && iptables -A INPUT -p tcp --dport 6001 -j ACCEPT && nohup python run.py > run.log &
+virtualenv -p "/usr/bin/python3.6" dataone_env && source "dataone_env/bin/activate" && pip3 install -r requirements.txt && iptables -A INPUT -p tcp --dport 6001 -j ACCEPT && pm2 start run.py --name dataoneapi &
